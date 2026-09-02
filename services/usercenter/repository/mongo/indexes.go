32668138c87d000869df93b97b72ac6386c05e20
J-Johnny <737759952@qq.com>
2026-09-02 10:53:45 +0800

    Fix MongoDB compound index definitions

--- services/usercenter/repository/mongo/indexes.go
+++ services/usercenter/repository/mongo/indexes.go
@@ -2,38 +2,95 @@ package mongo
 
 import (
 	"context"
+
+	"go.mongodb.org/mongo-driver/bson"
 	driverMongo "go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 func accountIndexes() []driverMongo.IndexModel {
 	return []driverMongo.IndexModel{
-		{Keys: map[string]int{"account_id": 1}, Options: options.Index().SetUnique(true)},
+		{
+			Keys: bson.D{
+				{Key: "account_id", Value: 1},
+			},
+			Options: options.Index().SetUnique(true),
+		},
 	}
 }
 
 func identityIndexes() []driverMongo.IndexModel {
 	return []driverMongo.IndexModel{
-		{Keys: map[string]int{"identity_id": 1}, Options: options.Index().SetUnique(true)},
-		{Keys: map[string]int{"provider": 1, "subject": 1}, Options: options.Index().SetUnique(true)},
-		{Keys: map[string]int{"account_id": 1}},
+		{
+			Keys: bson.D{
+				{Key: "identity_id", Value: 1},
+			},
+			Options: options.Index().SetUnique(true),
+		},
+		{
+			Keys: bson.D{
+				{Key: "provider", Value: 1},
+				{Key: "subject", Value: 1},
+			},
+			Options: options.Index().SetUnique(true),
+		},
+		{
+			Keys: bson.D{
+				{Key: "account_id", Value: 1},
+			},
+		},
 	}
 }
 
 func refreshTokenIndexes() []driverMongo.IndexModel {
 	return []driverMongo.IndexModel{
-		{Keys: map[string]int{"token_id": 1}, Options: options.Index().SetUnique(true)},
-		{Keys: map[string]int{"token_hash": 1}, Options: options.Index().SetUnique(true)},
-		{Keys: map[string]int{"account_id": 1, "install_id": 1}},
-		{Keys: map[string]int{"expires_at": 1}, Options: options.Index().SetExpireAfterSeconds(0)},
+		{
+			Keys: bson.D{
+				{Key: "token_id", Value: 1},
+			},
+			Options: options.Index().SetUnique(true),
+		},
+		{
+			Keys: bson.D{
+				{Key: "token_hash", Value: 1},
+			},
+			Options: options.Index().SetUnique(true),
+		},
+		{
+			Keys: bson.D{
+				{Key: "account_id", Value: 1},
+				{Key: "install_id", Value: 1},
+			},
+		},
+		{
+			Keys: bson.D{
+				{Key: "expires_at", Value: 1},
+			},
+			Options: options.Index().SetExpireAfterSeconds(0),
+		},
 	}
 }
 
 func idempotencyIndexes() []driverMongo.IndexModel {
 	return []driverMongo.IndexModel{
-		{Keys: map[string]int{"key": 1}, Options: options.Index().SetUnique(true)},
-		{Keys: map[string]int{"expires_at": 1}, Options: options.Index().SetExpireAfterSeconds(0)},
-		{Keys: map[string]int{"operation": 1, "created_at": -1}},
+		{
+			Keys: bson.D{
+				{Key: "key", Value: 1},
+			},
+			Options: options.Index().SetUnique(true),
+		},
+		{
+			Keys: bson.D{
+				{Key: "expires_at", Value: 1},
+			},
+			Options: options.Index().SetExpireAfterSeconds(0),
+		},
+		{
+			Keys: bson.D{
+				{Key: "operation", Value: 1},
+				{Key: "created_at", Value: -1},
+			},
+		},
 	}
 }
 
